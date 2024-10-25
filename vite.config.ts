f15bf0419bd567e32439488c9ae263e43c9bd91e
Radoslav Pavlinchov <radoslav.pavlinchov@limechain.tech>
2024-10-25 11:00:47 +0300

    fix: resolve node buffer issue

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+// import { NodeGlobalsPolyfillPlugin } from "@esbuild-plugins/node-globals-polyfill";
+// import { polyfillNode } from "esbuild-plugin-polyfill-node";
+import { nodePolyfills } from "vite-plugin-node-polyfills";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [nodePolyfills(), react()],
+});
